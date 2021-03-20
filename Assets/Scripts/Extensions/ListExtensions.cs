43428cf4157969a764fa905d40258d83b5961e08
eyeq <eyeq800@gmail.com>
2021-03-20 15:57:16 +0900

    Add Scene DrawCard 手札のドロー処理

---
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class ListExtensions
+    {
+        public static void Swap<T>(this IList<T> list, int i, int j)
+        {
+            var tmp = list[i];
+            list[i] = list[j];
+            list[j] = tmp;
+        }
+        
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            for (var i = list.Count - 1; i > 0; i--) {
+                var j = Random.Range(0, i + 1);
+                list.Swap(i, j);
+            }
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Controller;
 using Event;
 using Extensions;
@@ -12,18 +14,38 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] public Transform filedPanel;
 
+    private readonly List<int> _deck = new List<int>();
+
     public void Start()
     {
         cardPrefab.gameObject.RemoveAllComponents<CardDraggable>();
         cardPrefab.gameObject.AddComponent<CardDraggable>().fieldPanel = filedPanel;
-        
+
+        _deck.Clear();
+        _deck.AddRange(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13});
+        _deck.Shuffle();
+
         for (var i = 0; i < 7; i++)
         {
-            var card = Instantiate(cardPrefab, handPanel);
-            card.Init(new CardModel
-            {
-                Num = i,
-            });
+            DrawCard();
         }
+    }
+
+    public void DrawCard()
+    {
+        if (!_deck.Any())
+        {
+            return;
+        }
+
+        var cardEntity = _deck.First();
+
+        var card = Instantiate(cardPrefab, handPanel);
+        card.Init(new CardModel
+        {
+            Num = cardEntity,
+        });
+
+        _deck.RemoveAt(0);
     }
 }
